50be967eca4a0f8e4190497018e6a18c41ad5c16
Ervin Natareno <ean1207@gmail.com>
2021-05-12 17:42:03 -0700

    Installed Dependencies & Added Links and Router

---
+++ frontend/public/components/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css';
+import {Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            {/* Logo */}
+            <div className='navbar__logo'>
+                <h2>MERN Shopping Cart</h2>
+            </div>
+
+            {/* Links */}
+            <ul className='navbar__links'>
+                <li>
+                    <Link to='/cart'>
+
+                    </Link>
+                </li>
+            </ul>
+            {/* Hamburger menu */}
+        </nav>
+    )
+};
+
+export default Navbar;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import './ProductScreen.css';
+
+const ProductScreen = () => {
+    return (
+        <div className='productscreen'>
+            Products Screen
+        </div>
+    );
+};
+
+export default ProductScreen;
