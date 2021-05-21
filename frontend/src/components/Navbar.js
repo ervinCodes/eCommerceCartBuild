e9761b7cb7d5fa6c7de478b4ae8d8e42c008c098
Ervin Natareno <ean1207@gmail.com>
2021-05-20 17:50:01 -0700

    Navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import './Navbar.css';
+import {Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            {/* Logo */}
+            <div className='navbar__logo'>
+                <h2>MERN Shopping Cart</h2>
+            </div>
+
+            {/* Links */}
+            <ul className='navbar__links'>
+                <li>
+                    <Link to='/cart' className='cart__link'>
+                        {/* Icon */}
+                        <i className='fas fa-shopping-cart'></i>
+                        <span>
+                            Cart
+                            <span className='cartlogo__badge'>0</span>
+                        </span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/'>
+                        Shop
+                    </Link>
+                </li>
+            </ul>
+            {/* Hamburger menu */}
+            <div className='hamburger__menu'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
